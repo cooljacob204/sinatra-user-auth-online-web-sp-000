14a55cdf2f4b9c8876ca2457eba361d0bacb83e2
Jacob Feldman <jacobgfeld@gmail.com>
2019-03-28 23:27:51 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    p test
+    p 'test'
     @user = User.find_by(email: params["email"], password: params["password"])
     if @user
       session[:user_id] = @user.id
