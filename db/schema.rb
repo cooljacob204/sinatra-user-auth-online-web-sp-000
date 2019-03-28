3847ef4070ee0f54dd906305fda287b01cce4e85
Jacob Feldman <jacobgfeld@gmail.com>
2019-03-28 23:06:37 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 1) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "string"
+    t.string   "email"
     t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
