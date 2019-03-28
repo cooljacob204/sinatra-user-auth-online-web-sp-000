1e3694b9eafc670672daacdc4ababe4b8cefa365
Jacob Feldman <jacobgfeld@gmail.com>
2019-03-28 23:23:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,9 +32,9 @@ class ApplicationController < Sinatra::Base
   post '/session/login' do
     user = User.find_by_id(params["email"])
     
-    session[:user_id] = user.id if user.password == params["password"]
-    
-    redirect '/users/home'
+    if user.password == params["password"]
+      session[:user_id] = user.id 
+      redirect '/users/home'
   end
 
   post '/sessions' do
