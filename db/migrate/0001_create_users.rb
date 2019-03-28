63f9fa0e75c2a8f76e9d1c046a53f1e346555dc3
Jacob Feldman <jacobgfeld@gmail.com>
2019-03-28 23:03:57 +0000

    Automatically backed up by Learn

--- db/migrate/0001_create_users.rb
+++ db/migrate/0001_create_users.rb
@@ -1,5 +1,11 @@
-
-class AddPartNumberToProducts < ActiveRecord::Migration[5.0]
+class CreateUsers < ActiveRecord::Migration[5.0]
   def change
+    create_table :users do |t|
+      t.string :name
+      t.string :string
+      t.string :password
+ 
+      t.timestamps
+    end
   end
 end
